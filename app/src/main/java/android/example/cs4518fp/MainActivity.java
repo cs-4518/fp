c7944539b5ce8f0ac1d7ac36309fad9d33b5b1c9
Elie <eliemhess@gmail.com>
2020-02-11 09:43:25 -0500

    Add TarsosDSP

--- app/src/main/java/android/example/cs4518fp/MainActivity.java
+++ app/src/main/java/android/example/cs4518fp/MainActivity.java
@@ -1,13 +1,93 @@
 package android.example.cs4518fp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mNoteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("TEST", "reached point 1");
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d("TEST", "reached point 2");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+                    123);
+        } else {
+            Log.d("TEST", "reached point 3");
+        }
+        Log.d("TEST", "reached point 4");
+
+        AudioDispatcher dispatcher =
+                AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
+
+        mNoteText = findViewById(R.id.noteText);
+
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult res, AudioEvent e) {
+                final float pitchInHz = res.getPitch();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        processPitch(pitchInHz);
+                    }
+                });
+            }
+        };
+        AudioProcessor pitchProcessor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(pitchProcessor);
+
+        Thread audioThread = new Thread(dispatcher, "Audio Thread");
+        audioThread.start();
+    }
+
+    public void processPitch(float pitchInHz) {
+
+        mNoteText.setText(String.format(Locale.getDefault(), "%f", pitchInHz));
+
+        if (pitchInHz >= 110 && pitchInHz < 123.47) {
+            //A
+            mNoteText.setText("A");
+        } else if (pitchInHz >= 123.47 && pitchInHz < 130.81) {
+            //B
+            mNoteText.setText("B");
+        } else if (pitchInHz >= 130.81 && pitchInHz < 146.83) {
+            //C
+            mNoteText.setText("C");
+        } else if (pitchInHz >= 146.83 && pitchInHz < 164.81) {
+            //D
+            mNoteText.setText("D");
+        } else if (pitchInHz >= 164.81 && pitchInHz <= 174.61) {
+            //E
+            mNoteText.setText("E");
+        } else if (pitchInHz >= 174.61 && pitchInHz < 185) {
+            //F
+            mNoteText.setText("F");
+        } else if (pitchInHz >= 185 && pitchInHz < 196) {
+            //G
+            mNoteText.setText("G");
+        }
     }
 }
